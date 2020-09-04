fd387f83f717ce479e3ad001167c808ea87ea658
maghell <70374541+maghell@users.noreply.github.com>
2020-09-04 12:53:02 +0200

    pladen er blevet ferdig

--- test.pi.py
+++ test.pi.py
@@ -1,33 +1,51 @@
 from turtle import *
-speed (1000)
-pu()
-goto (-300,150)
-pd()
-#her tinger jeg firkanten
-for i in range(2):
-    fd (600)
-    rt (90)
-    fd (400)
-    rt (90)
-speed (1000)
-#her setter jeg hvor den skal tainge fra
-for i in range (5):
-    pu ()
-    rt (90)
-    fd (75)
-    lt (90)
-    fd (50)
-    pd ()
-#her tinger jeg selve cirklarne
-    for i in range(10):
-        circle(25,360,30)
+def setup (): 
+    speed (1000)
+    pu()
+    goto (-300,150)
+    pd()
+    #her tinger jeg firkanten
+    for i in range(2):
+        fd (600)
+        rt (90)
+        fd (400)
+        rt (90)
+    speed (1000)
+    #her setter jeg hvor den skal tainge fra
+    for i in range (5):
         pu ()
-        fd (55)
-        pd()
-#her venner jeg tilbage
-    pu ()
-    rt (180)
-    fd (600)
-    rt(180)
+        rt (90)
+        fd (75)
+        lt (90)
+        fd (50)
+        pd ()
+    #her tinger jeg selve cirklarne
+        for i in range(10):
+            circle(25,360,30)
+            pu ()
+            fd (55)
+            pd()
+    #her venner jeg tilbage
+        pu ()
+        rt (180)
+        fd (600)
+        rt(180)
 
+    column = []
+    for i in range (10):
+        row = []
+        for i in range (5):
+            row.append(0)
+        column.append(row)
+
+def coulor(column,coulor):
+    fillcolor (coulor) 
+    fd (75)
+    circle(25,360,30)
     
+
+
+setup ()    
+
+coulor (8,"red")
+
